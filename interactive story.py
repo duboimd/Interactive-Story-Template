a6aa1780d492b97a48a21f8470f23396631ba57f
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-10-31 13:03:50 -0600

    Create interactive story.py

---
+++ interactive story.py
@@ -0,0 +1,4 @@
+"""
+You will create an interactive story that will display blocks of text, allow users to make a choice to advance the story, and have at least 5 different endings.
+Use if elif else statements. Use input from the user. Don't forget to use string operators like .upper() to help optimize your code
+"""
